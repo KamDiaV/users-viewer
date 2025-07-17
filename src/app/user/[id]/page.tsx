f73737f6ec7a8dc64934b2b6f43509584039ead1
Diana Stepanova <diana@MacBook-Air-Diana.local>
2025-07-17 22:38:56 +0500

    remove comments

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,5 +1,3 @@
-// src/app/user/[id]/page.tsx
-
 import { getUser } from '@/services/users'
 import { notFound } from 'next/navigation'
 import UserDetailContent from '@/components/UserDetailContent'
@@ -9,7 +7,6 @@ export default async function UserPage({
 }: {
   params: Promise<{ id: string }>
 }) {
-  // согласно новой модели Dynamic APIs в Next 15+, нужно await
   const { id } = await params
 
   const user = await getUser(id)
--- src/components/layout/PageTransitionWrapper.tsx
+++ src/components/layout/PageTransitionWrapper.tsx
@@ -15,11 +15,8 @@ export function PageTransitionWrapper({
       <AnimatePresence mode="wait" initial={false}>
         <motion.div
           key={pathname}
-          // не скрываем старый контент
           exit={{ opacity: 1 }}
-          // не делаем initial анимацию при первом рендере
           initial={false}
-          // делаем плавный вход нового
           animate={{ opacity: 1 }}
           transition={{ duration: 0.2 }}
           style={{ position: "absolute", width: "100%", top: 0, left: 0 }}
